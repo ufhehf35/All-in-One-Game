fb302b518d5f76c028871c9e65ef233e9d3f1024
ufhehf35 <111770162+ufhehf35@users.noreply.github.com>
2026-09-13 16:11:47 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -98,7 +98,7 @@ let gameData = {
         },
         {
             name: "بوكو نو هيرو",
-            image: "images/bukunoherp.jpg",
+            image: "images/bukunohero.jpg",
             questions: [
                 { points: 300, q: "ما اسم البطل الرئيسي الذي ولد بدون قدرة؟", a: "إيزوكو ميدوريا (ديكو)" },
                 { points: 300, q: "ما اسم البطل رقم واحد ومعلم ميدوريا؟", a: "أولمايت" },
